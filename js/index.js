abe62a39e98d78c9814b60553b4b6c0b3e0dd0ff
Jawad Rashid <nokia.2003@gmail.com>
2016-08-31 20:31:38 +0500

    Formatting Done.

--- js/index.js
+++ js/index.js
@@ -1,24 +1,24 @@
 $(".mainnav a").on("click", function() {
-  $(".mainnav").find(".active").removeClass("active");
-  $(this).parent().addClass("active");
+    $(".mainnav").find(".active").removeClass("active");
+    $(this).parent().addClass("active");
 });
 
 $(document).ready(function() {
-  //$('[data-toggle="tooltip"]').tooltip();
-  $('[data-toggle="tooltip"]').tooltip({
-    track: true
-  });
-  $('[data-toggle="skills"]').tooltip({
-    track: true
-  });
-  $('body').scrollspy({
-    target: '#navbar-div',
-    offset: 50
-  })
+    //$('[data-toggle="tooltip"]').tooltip();
+    $('[data-toggle="tooltip"]').tooltip({
+        track: true
+    });
+    $('[data-toggle="skills"]').tooltip({
+        track: true
+    });
+    $('body').scrollspy({
+        target: '#navbar-div',
+        offset: 50
+    })
 });
 
 var shiftWindow = function() {
-  scrollBy(0, -50)
+    scrollBy(0, -50)
 };
 if (location.hash) shiftWindow();
 window.addEventListener("hashchange", shiftWindow);
